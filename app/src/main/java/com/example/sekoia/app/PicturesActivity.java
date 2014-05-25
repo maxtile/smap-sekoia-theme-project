3b44c54192edbe8e922e92fc8df3161c1839edd5
Maximilian Wiese <maximilian_wiese@hotmail.de>
2014-05-25 23:57:35 +0200

    Tilfoejet permission spørgsel til kamera og Storage. Added Activity class and Fragment class. Added Interface for communication from fragment to activity.

---
+++ app/src/main/java/com/example/sekoia/app/PicturesActivity.java
@@ -0,0 +1,56 @@
+package com.example.sekoia.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PicturesActivity extends ActionBarActivity implements PicturesFragment.OnPicturesFragmentInteraction{
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pictures);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PicturesFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.pictures, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTakePictureButtonClicked() {
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sekoia/app/PicturesFragment.java
@@ -0,0 +1,49 @@
+package com.example.sekoia.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Maximilian on 25.05.14.
+ */
+public class PicturesFragment extends Fragment {
+    private OnPicturesFragmentInteraction aCallback;
+    private Button takePicButton;
+
+    public PicturesFragment() {
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            aCallback = (OnPicturesFragmentInteraction) activity;
+        } catch (ClassCastException exception){
+            throw new ClassCastException(activity.toString() + "must implement " +
+                    "OnOnPicturesFragmentInteraction");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_pictures, container, false);
+        takePicButton = (Button)rootView.findViewById(R.id.takePictureButton);
+        takePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aCallback.onTakePictureButtonClicked();
+            }
+        });
+        return rootView;
+    }
+
+    public interface OnPicturesFragmentInteraction{
+        public void onTakePictureButtonClicked();
+    }
+}
