c453455047a0d5cae959cf6c72e47673a10ed77b
Lennart Kjær <lnkjaer1@gmail.com>
2014-06-01 17:37:14 +0200

    json data m. bound service

--- app/src/main/java/com/example/sekoia/app/RelativesChooseActivity.java
+++ app/src/main/java/com/example/sekoia/app/RelativesChooseActivity.java
@@ -1,8 +1,15 @@
 package com.example.sekoia.app;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -11,11 +18,11 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.sekoia.app.Service.GetRelativesService;
 import com.example.sekoia.app.dummy.RelativeModel;
 import java.util.ArrayList;
 import java.util.List;
-
-
 
 
 public class RelativesChooseActivity extends Activity {
@@ -24,31 +31,38 @@ public class RelativesChooseActivity extends Activity {
     public final static String MESSAGE_ID = "com.example.sekoia.app.MESSAGE";
     public final static String MESSAGE_FULLNAME = "com.example.sekoia.app.MESSAGE";
 
+    GetRelativesService mService;
+    boolean mBound = false;
+    private List<RelativeModel> model = new ArrayList<RelativeModel>();
+    ArrayAdapter<RelativeModel> mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relatives);
 
-        populateRelativesList();
+        //mAdapter = new ArrayAdapter<String>(this, android.R.layout.activity_relatives, model);
+        //setListAdapter(mAdapter);
+
+        //populateRelativesList();
         populatListview();
         registerClickCallback();
         }
 
-    private List<RelativeModel> model = new ArrayList<RelativeModel>();
+   // private void populateRelativesList(){
+   //     model.add(new RelativeModel(111, "Kalle", "Hansen",R.drawable.kalle, "Grenen", "Lejlighed 3"));
+   // }
 
-    private void populateRelativesList(){
-        model.add(new RelativeModel(111, "Kalle", "Hansen",R.drawable.kalle, "Grenen", "Lejlighed 3"));
-        model.add(new RelativeModel(222, "Nancy", "Berggren", R.drawable.nancy, "Kvisten", "Lejlighed 14"));
-    }
+    //private void populateRelativesList(){
+    //    model.add(new RelativeModel(111, "Kalle", "Hansen",R.drawable.kalle, "Grenen", "Lejlighed 3"));
+    //    model.add(new RelativeModel(222, "Nancy", "Berggren", R.drawable.nancy, "Kvisten", "Lejlighed 14"));
+    //}
 
-    private void populatListview(){
+    public void populatListview(){
         ArrayAdapter<RelativeModel> adapter = new MyListAdapter();
         ListView list = (ListView) findViewById(R.id.relativesListView);
         list.setAdapter(adapter);
     }
-
-
-
 
     private void registerClickCallback(){
         ListView list = (ListView) findViewById(R.id.relativesListView);
@@ -60,14 +74,10 @@ public class RelativesChooseActivity extends Activity {
 
                 // fire intent to start new activity
                 startMenuActivity(relativemodel);
-
-
-
             }
         });
 
     }
-
 
     private void startMenuActivity(RelativeModel relativeModel){
 
@@ -82,8 +92,6 @@ public class RelativesChooseActivity extends Activity {
         startActivity(intent);
 
     }
-
-
 
     public class MyListAdapter extends ArrayAdapter<RelativeModel>{
         public MyListAdapter(){
@@ -119,18 +127,100 @@ public class RelativesChooseActivity extends Activity {
 
             return itemView;
         }
+    }
 
+    //Kode for service
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, GetRelativesService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 
+        if(mBound) {
+            mService.startDownload();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, new IntentFilter(GetRelativesService.NOTIFICATION));
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
     }
 
 
 
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            GetRelativesService.IProtocolBinder binder = (GetRelativesService.IProtocolBinder) service;
+            mService = binder.getService();
+            mBound = true;
+            mService.startDownload();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                int resultCode = bundle.getInt(GetRelativesService.RESULT);
+                if(resultCode == GetRelativesService.RESULT_OK) {
+                    if(mBound) {
+                        model = mService.getData();
+                        populatListview();
+                        //model = getTitles(mService.getData());
+                        //mAdapter = new ArrayAdapter<RelativesChooseActivity>(RelativesChooseActivity.this, android.R.layout.activity_relatives, model);
+                        //RelativesChooseActivity.this.setListAdapter(mAdapter);
+
+                    }
+                }else{
+                    Toast.makeText(RelativesChooseActivity.this, "Error downloading", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    };
+
+
+    /*private List<String> getTitles(List<RelativeModel> relatives) {
+        ArrayList<RelativeModel> result = new ArrayList<RelativeModel>();
+        for(RelativeModel s : relatives) {
+            //result.add(s.getLastName());
+            //model.add(s.getFirstName());
+
+            //result.add(s.getLastName());
+        }
+        return result;*/
+    //}
 
 
 
 
 
-    }
+}
 
 
 
---
+++ app/src/main/java/com/example/sekoia/app/Service/GetRelativesService.java
@@ -0,0 +1,115 @@
+package com.example.sekoia.app.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.IBinder;
+
+import com.example.sekoia.app.dummy.RelativeModel;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class GetRelativesService extends Service {
+
+    public static final String REST_SERVER_ENDPOINT = "http://demo3067547.mockable.io/";
+    public static final String NOTIFICATION = "receiver";
+    public static final String RESULT = "result";
+    public static final int RESULT_OK = 94215;
+    public static final int RESULT_FAILED = 2366316;
+
+    private List<RelativeModel> mList;
+    private final IBinder mBinder = new IProtocolBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    public class IProtocolBinder extends Binder {
+        public GetRelativesService getService() {
+            return GetRelativesService.this;
+        }
+    }
+
+    public void startDownload() {
+        new DownloadTask().execute();
+    }
+
+    public List<RelativeModel> getData() {
+        return mList;
+    }
+
+    private void notifyDownloadDone(int result) {
+        Intent intent = new Intent(NOTIFICATION);
+        intent.putExtra(RESULT, result);
+        sendBroadcast(intent);
+    }
+
+    private class DownloadTask extends AsyncTask<Void, Integer, List<RelativeModel>> {
+
+        @Override
+        protected List<RelativeModel> doInBackground(Void... args) {
+            String json = getJSONRaw();
+            if(json == null) return null;
+            Gson gson = new Gson();
+            Type listType = new TypeToken<List<RelativeModel>>() {}.getType();
+            List<RelativeModel> results = gson.fromJson(json, listType);
+            return results;
+        }
+
+        protected void onPostExecute(List<RelativeModel> result) {
+            if(result == null) {
+                notifyDownloadDone(RESULT_FAILED);
+            }else{
+                mList = result;
+                notifyDownloadDone(RESULT_OK);
+            }
+        }
+
+        public String getJSONRaw() {
+            StringBuilder builder = new StringBuilder();
+            HttpClient client = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(REST_SERVER_ENDPOINT);
+            try {
+                HttpResponse response = client.execute(httpGet);
+                StatusLine statusLine = response.getStatusLine();
+                int statusCode = statusLine.getStatusCode();
+                if (statusCode == 200) {
+                    HttpEntity entity = response.getEntity();
+                    InputStream content = entity.getContent();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                    }
+                } else {
+                    return null;
+                }
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return builder.toString();
+        }
+    }
+
+
+
+}
